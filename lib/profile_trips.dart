039604234ae2eacb3a85a2bffe2773d94585b726
Tenki <srodriguezdev.l@gmail.com>
2023-01-25 11:25:02 -0500

    Se arreglo la card de profile, y se esta trabajando en la lista reutilizable

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -4,23 +4,30 @@ class ProfileTrips extends StatelessWidget {
   String mainTitleCard;
   String placeName;
   String steps;
+  String pathImageOne = "assets/images/imageOne.jpg";
 
-  ProfileTrips(this.mainTitleCard, this.placeName, this.steps, {super.key});
+  ProfileTrips(
+      this.mainTitleCard, this.placeName, this.steps, this.pathImageOne,
+      {super.key});
 
   @override
   Widget build(BuildContext context) {
     final profileCard = Center(
       child: Container(
-        height: 150.0,
         width: 350.0,
-        margin: const EdgeInsets.only(top: 80.0),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(
+        height: 220.0,
+        margin: const EdgeInsets.only(top: 80.0, left: 10.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(pathImageOne),
+          ),
+          // color: const Color.fromARGB(255, 136, 134, 134),
+          borderRadius: const BorderRadius.all(
             Radius.circular(30.0),
           ),
           shape: BoxShape.rectangle,
-          boxShadow: [
+          boxShadow: const [
             BoxShadow(
               color: Colors.black38,
               blurRadius: 15.0,
@@ -68,19 +75,38 @@ class ProfileTrips extends StatelessWidget {
       ),
     );
 
-    final descriptionCard = Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [titleProfileCard, descriptionPlace, stepsWalking],
+    final descriptionBox = Center(
+      child: Container(
+        width: 280.0,
+        height: 120.0,
+        margin: const EdgeInsets.only(top: 280.0, left: 10.0),
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+          shape: BoxShape.rectangle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black38,
+              blurRadius: 15.0,
+              offset: Offset(0.7, 7.0),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [titleProfileCard, descriptionPlace, stepsWalking],
+            ),
+          ),
+        ),
+      ),
     );
 
-    final cardProfileZones = Stack(
-      children: [
-        descriptionCard,
-      ],
-    );
-
-    return Row(
-      children: [cardProfileZones],
+    return Stack(
+      children: [profileCard, descriptionBox],
     );
   }
 }
