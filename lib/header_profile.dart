62ee50aac17150b0b1c728062766c72f11c10256
Tenki <srodriguezdev.l@gmail.com>
2023-01-25 11:25:46 -0500

    Se adiciono un archivo donde se estara trabajando el header

---
+++ lib/header_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HeaderProfile extends StatefulWidget {
+  const HeaderProfile({super.key});
+
+  @override
+  State<HeaderProfile> createState() => _HeaderProfileState();
+}
+
+class _HeaderProfileState extends State<HeaderProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
