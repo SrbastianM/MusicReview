4e83ea3dacadb4afd297f3686d2b7a664738a04b
Tenki <srodriguezdev.l@gmail.com>
2023-05-10 12:23:32 -0500

    Ajustamos las carpetas a subcarpetas para una mejor distribucoin

--- lib/presentation/header_profile.dart
+++ lib/presentation/header_profile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'gradiant_profile_back.dart';
-import 'header_profile_appbar.dart';
+import '../domain/gradiant_profile_back.dart';
+import '../domain/header_profile_appbar.dart';
 
 class HeaderProfile extends StatelessWidget {
   const HeaderProfile({super.key});
